3219c0d755754debd8daf9a561c8899189ca76a7
Kukuri <83948007+Mikasuru@users.noreply.github.com>
2024-10-23 23:10:17 +0700

    Add some stupid function
    Im really messed up

--- src/World/World.cpp
+++ src/World/World.cpp
@@ -1,1 +1,32 @@
 #include <World/World.hpp>
+
+World::World() {
+    // constructor
+}
+
+World::~World() {
+    m_players.clear();
+}
+
+void World::AddPlayer(uint32_t connectId, Player* player) {
+    if (!player) return;
+    m_players[connectId] = player;
+}
+
+void World::RemovePlayer(uint32_t connectId) {
+    if (m_players.find(connectId) != m_players.end()) {
+        m_players.erase(connectId);
+    }
+}
+
+Player* World::GetPlayer(uint32_t connectId) {
+    auto it = m_players.find(connectId);
+    if (it == m_players.end()) {
+        return nullptr;
+    }
+    return it->second;
+}
+
+size_t World::GetPlayerCount() const {
+    return m_players.size();
+}
