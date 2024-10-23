ea23c3521fa11b40f6bb751aa630554b5c8ea99c
Kukuri <83948007+Mikasuru@users.noreply.github.com>
2024-10-23 23:16:29 +0700

    Update WorldPool.hpp

--- src/World/WorldPool.hpp
+++ src/World/WorldPool.hpp
@@ -2,8 +2,8 @@
 #include <memory>
 #include <unordered_map>
 #include <World/World.hpp>
+#include <Player/Player.hpp>
 #include <string>
-#include <unordered_map>
 
 class WorldPool {
 public:
@@ -17,6 +17,9 @@ public:
     std::shared_ptr<World> NewWorld(std::string vName);
     void RemoveWorld(std::string vName);
     std::shared_ptr<World> GetWorld(std::string vName);
+
+    void SendToWorldSelect(Player* pPlayer);
+    void JoinWorld(Player* pPlayer, const std::string& worldName);
 
 private:
     std::unordered_map<std::string, std::shared_ptr<World>> m_worlds{};
